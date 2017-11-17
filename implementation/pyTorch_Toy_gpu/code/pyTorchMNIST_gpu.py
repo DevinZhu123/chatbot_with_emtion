LSTM with average pooling layer -- fixed sentence length

implementation/pyTorch_Toy_gpu/code/pyTorchMNIST_gpu.py
@@ -22,7 +22,7 @@ class demoLSTM(nn.Module):
         self.embedding_dim = embedding_dim
         self.LSTM = nn.LSTMCell(embedding_dim, hidden_dim)
         self.hidden = self.init_LSTMhidden()
-        self.poolsize = 3
+        self.poolsize = 4
         self.stride = 2
         pool_output_h = math.floor((self.hidden_dim - self.poolsize) / self.stride + 1)
         pool_output_w = math.floor((28 - self.poolsize) / self.stride + 1)
@@ -49,31 +49,47 @@ class demoLSTM(nn.Module):
             lstm_output.append(eh)
         pooling_input = torch.stack(lstm_output, 1)
         pooling_output = self.avgpooling(pooling_input).view(self.batch_size, -1)
-        # print(pooling_input.size())
-        # print(pooling_output.size())
+
         cls_space = self.hidden2tag(pooling_output)
         cls_score = F.log_softmax(cls_space)
         return cls_score
 
 
 def loadData(path_to_tweet, path_to_tag, path_to_word_vec):
-    # load MNIST
+    # load
     dataX, dataY = utils.load_tweet_data(path_to_tweet, path_to_tag, path_to_word_vec)
-    tX = torch.from_numpy(dataX).float()
-    tY = torch.from_numpy(dataY)
-    return Variable(tX.cuda()), Variable(tY.cuda())
+    tX = torch.from_numpy(dataX[0:dataX.shape[0]*4/5]).float()
+    tY = torch.from_numpy(dataY[0:dataX.shape[0]*4/5])
+    vX = torch.from_numpy(dataX[dataX.shape[0]*4/5:]).float()
+    vY = torch.from_numpy(dataY[dataX.shape[0]*4/5:])
+    return Variable(tX.cuda()), Variable(tY.cuda()), Variable(vX.cuda()), Variable(vY.cuda())
 
 
 def testDemoTweets():
-    batch_size = 200
-    model = demoLSTM(batch_size=batch_size, embedding_dim=50, hidden_dim=200, targetSize=7).cuda()
-    loss_function = nn.NLLLoss().cuda()
-    optimizer = optim.SGD(model.parameters(), lr=0.05)
-    # addr = "../data/digitstrain.txt"
     path_to_tweet = '../../../data/TweetsData/train_1/cleanTrain_1Tweets.txt'
     path_to_tag = '../../../data/TweetsData/train_1/tweets_with_tags_train_1_IdMoods.txt'
     path_to_word_vec = '../../../data/vector/glove.6B/glove.6B.50d.txt'
-    dataX, dataY = loadData(path_to_tweet, path_to_tag, path_to_word_vec)
+    dataX, dataY, validX, validY = loadData(path_to_tweet, path_to_tag, path_to_word_vec)
+
+    # add some test cases
+    positive_data, _ = utils.load_tweet_data('../data/clearEmotion_positive.txt', path_to_tag, path_to_word_vec, seed=1)
+    anger_data, _ = utils.load_tweet_data('../data/clearEmotion_anger.txt', path_to_tag, path_to_word_vec, seed=1)
+    positive_data = Variable(torch.from_numpy(positive_data).float().cuda())
+    anger_data = Variable(torch.from_numpy(anger_data).float().cuda())
+
+
+
+    batch_size = 1
+    model = demoLSTM(batch_size=batch_size, embedding_dim=50, hidden_dim=200, targetSize=3).cuda()
+    loss_function = nn.NLLLoss().cuda()
+    optimizer = optim.SGD(model.parameters(), lr=0.1, weight_decay=0.001)
+
+    torch.save(model.state_dict(), './trained_model')
+
+
+
+
+
     # test loaded data
     print "loaded data", dataX[0:batch_size].size()
     # test model
@@ -83,7 +99,7 @@ def testDemoTweets():
     # train model
     losses = []
     iters = 0
-    for epoch in range(500):
+    for epoch in range(2):
         print "epoch: ", epoch+1
         for i in range(dataX.size()[0]/batch_size):
             iters += 1
@@ -95,8 +111,9 @@ def testDemoTweets():
             loss = loss_function(tag_score, tag)
             loss.backward()
             optimizer.step()
-            losses.append(loss.data.cpu().numpy()[0])
-            if iters % 100 == 0:
+
+            if iters % 1000 == 0:
+                losses.append(loss.data.cpu().numpy()[0])
                 print "loss", loss.data.cpu().numpy()[0]
     print "finish training"
     print "test overfit"
@@ -117,7 +134,50 @@ def testDemoTweets():
         d1 += np.sum(pred == tag)
     print "train accuracy", d1 * 1.0 / d2
 
+    torch.save(model.state_dict(), './trained_model')
+
+
+
+    for i in range(positive_data.size()[0] / batch_size):
+        input = positive_data[i*batch_size:(i+1)*batch_size]
+        model.zero_grad()
+        model.hidden = model.init_LSTMhidden()
+        tag_score = model(input)
+        pred = np.argmax(tag_score.data.cpu().numpy(), axis=1)
+        print pred
+
+    print '------------------'
+
+    for i in range(anger_data.size()[0] / batch_size):
+        input = anger_data[i*batch_size:(i+1)*batch_size]
+        model.zero_grad()
+        model.hidden = model.init_LSTMhidden()
+        tag_score = model(input)
+        pred = np.argmax(tag_score.data.cpu().numpy(), axis=1)
+        print pred
+
+
+
+
     """
+    print "test validation"
+    d1 = 0
+    d2 = 0
+    for i in range(validX.size()[0]/batch_size):
+        d2 += batch_size
+        img = validX[i*batch_size:(i+1)*batch_size]
+        tag = validY[i*batch_size:(i+1)*batch_size].data.cpu().numpy()
+        model.zero_grad()
+        model.hidden = model.init_LSTMhidden()
+        tag_score = model(img)
+        # print tag_score.data.cpu().numpy()
+        pred = np.argmax(tag_score.data.cpu().numpy(), axis=1)
+        # print(pred.shape, tag.shape)
+        assert pred.shape == tag.shape, 'prediction shape error.'
+        d1 += np.sum(pred == tag)
+    print "valid accuracy", d1 * 1.0 / d2
+
+    
     print "test validation"
     addr = "../data/digitsvalid.txt"
     validX, validY = loadData(addr)

implementation/pyTorch_Toy_gpu/code/utils.py
@@ -3,12 +3,12 @@ import re
 
 mood_dict = {
     'joy': 0,
-    'love': 1,
-    'sadness': 2,
-    'anger': 3,
-    'fear': 4,
-    'thankfulness': 5,
-    'surprise': 6
+    'love': 0,
+    'sadness': 1,
+    'anger': 1,
+    'fear': 1,
+    'thankfulness': 2,
+    'surprise': 2
 }
 
 class MatchVector:
@@ -67,7 +67,8 @@ def load_tweet_data(path_to_tweet, path_to_tag, path_to_word_vec, seed=1):
             max_sentence_size = max(len(words), max_sentence_size)
             sentences.append(words)
 
-    max_sentence_size = max_sentence_size * 4 / 5
+    # max_sentence_size = max_sentence_size * 4 / 5
+    max_sentence_size = 28
     dataX = [mv.get_matrix(words, max_sentence_size) for words in sentences]
     dataX = np.array(dataX)
 